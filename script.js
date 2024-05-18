5007ff64014f4c84f7b41b33bba71ba1ec38b252
SANJAY <sktk9909@gmail.com>
2024-05-18 22:00:24 +0530

    18-05-2024 files

---
+++ script.js
@@ -0,0 +1,55 @@
+getWeatherData = async () => {
+  let cityName = searchQueryInput.value;
+  if (cityName) {
+    // console.log(cityName);
+    const now = new Date();
+    const localDateTime = now.toLocaleString();
+    console.log(localDateTime);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5fe36b192ffd1c36dffb6752bc1722b2`
+    );
+    response.json().then((data) => {
+      let currentWeather = data.weather[0].main;
+      let tempDegree = Math.trunc((data.main.temp - 273.15) * 10) / 10;
+      let humidity = data.main.humidity;
+      let country = data.sys.country;
+      let city = data.name;
+      var temp1 = "";
+      var temp2 = "";
+      if (currentWeather == "Rain") {
+        temp1 = "It's Raining Now";
+        temp2 = "   Don't forget your umberllas !";
+      } else if (currentWeather == "Clouds") {
+      } else if (currentWeather == "Clear") {
+      }
+      //   console.log(tempDegree);
+      weatherData.innerHTML = `
+     <div
+    class="card shadow rounded-3"
+    style="width: 20rem"
+    id="climateCard"
+  >
+    <div class="card-body">
+      <h4 class="card-title text-light">${temp1}</h4>
+      <h5 class="card-subtitle mb-2 text-light">
+        ${temp2}
+      </h5>
+      <img
+        src="./media/rain-icon.png"
+        alt=""
+        class="ms-2 w-25 climate-img"
+      />
+      <h4 class="card-title text-light weather-details">${city.toUpperCase()}</h4>
+      <h4 class="card-title text-light">${tempDegree}<sup>o</sup>C</h4>
+      <h5 class="card-subtitle mb-2 text-light">
+        ${localDateTime}
+      </h5>
+    </div>
+  </div>
+    `;
+    });
+  } else {
+    alert(`Please enter valid input`);
+  }
+};
+// https://api.openweathermap.org/data/2.5/weather?q=Kochi&appid=5fe36b192ffd1c36dffb6752bc1722b2
