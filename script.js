9a70d67ed8e90325089dd6e9ba86e75932327e16
SANJAY <sktk9909@gmail.com>
2024-05-19 20:02:49 +0530

    19th May v2

--- script.js
+++ script.js
@@ -1,25 +1,53 @@
+let isFunctionCalled = false;
+let intervalId;
 getWeatherData = async () => {
+  if (intervalId) {
+    clearInterval(intervalId);
+  }
+  isFunctionCalled = true;
   let cityName = searchQueryInput.value;
   if (cityName) {
     // console.log(cityName);
+    document.body.style.background = "none";
     const now = new Date();
     const localDateTime = now.toLocaleString();
     console.log(localDateTime);
     const response = await fetch(
-      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=5fe36b192ffd1c36dffb6752bc1722b2`
+      `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=d4b89c4b3c286935a01b731ef3bc8720&units=metric`
     );
     response.json().then((data) => {
+      console.log(data);
       let currentWeather = data.weather[0].main;
-      let tempDegree = Math.trunc((data.main.temp - 273.15) * 10) / 10;
+      // let currentWeather = "Snow";
+      console.log(currentWeather);
+      let tempDegree = data.main.temp;
       let humidity = data.main.humidity;
       let country = data.sys.country;
       let city = data.name;
+      let cityId = data.id;
       var temp1 = "";
       var temp2 = "";
       var bg = "";
-      if (currentWeather == "Rain") {
-        temp1 = "It's Raining Now";
-        temp2 = "   Don't forget your umberllas !";
+      var weatherIcon = "";
+      if (
+        currentWeather == "Rain" ||
+        currentWeather == "Drizzle" ||
+        currentWeather == "Thunderstorm"
+      ) {
+        var rainCategory = "";
+        temp2 = "Don't forget your umberllas !";
+        if (currentWeather == "Rain") {
+          rainCategory = "Raining";
+          weatherIcon = "./media/rain-icon.png";
+        } else if (currentWeather == "Drizzle") {
+          rainCategory = "Drizzling";
+          weatherIcon = "./media/rain-icon.png";
+        } else if (currentWeather == "Thunderstorm") {
+          rainCategory = "Thunderstorms";
+          temp2 = "Don't go outside !";
+          weatherIcon = "./media/thuderstorm.png";
+        }
+        temp1 = `It's ${rainCategory} Now`;
         bg = "./media/night.gif";
         // document.body.style.backgroundImage = "url('')";
         const style = document.createElement("style");
@@ -31,8 +59,9 @@ getWeatherData = async () => {
         document.head.appendChild(style);
       } else if (currentWeather == "Clouds") {
         temp1 = "It's Cloudy Today";
-        temp2 = "Don't leave your raincoats behind!";
+        temp2 = "Gray skies looming overhead";
         bg = "./media/night.gif";
+        weatherIcon = "./media/cloudy-2.png";
         const style = document.createElement("style");
         style.innerHTML = `
           body::before {
@@ -40,21 +69,72 @@ getWeatherData = async () => {
           }
         `;
         document.head.appendChild(style);
-        document.body.style.backgroundImage = "url('./media/cloud-1.jpg')";
       } else if (currentWeather == "Clear") {
         temp1 = "The Sun's Out Today";
-        temp2 = "Don't leave your sunglasses at home!";
+        temp2 = "Blue skies and sunshine";
         bg = "./media/summer.gif";
+        weatherIcon = "./media/sunny.png";
         const style = document.createElement("style");
         style.innerHTML = `
           body::before {
-            background-image: url('./media/sunny-4.jpg');
+            background-image: url('./media/sunny-3.jpg');
           }
         `;
         document.head.appendChild(style);
-        document.body.style.backgroundImage = "url('./media/cloud-1.jpg')";
+      } else if (currentWeather == "Snow") {
+        temp1 = "Snowflakes falling gently outside";
+        temp2 = "Bundle up, it's snowing today";
+        bg = "./media/winter.gif";
+        weatherIcon = "./media/snow.png";
+        const style = document.createElement("style");
+        style.innerHTML = `
+          body::before {
+            background-image: url('./media/winter-5.jpg');
+          }
+        `;
+        document.head.appendChild(style);
+      } else if (currentWeather == "Mist" || currentWeather == "Haze") {
+        temp1 = "Damp and misty outside";
+        temp2 = "Visibility reduced due to mist";
+        bg = "./media/winter_morning.gif";
+        weatherIcon = "./media/mist.png";
+        const style = document.createElement("style");
+        style.innerHTML = `
+          body::before {
+            background-image: url('./media/mist-haze-2.jpg');
+          }
+        `;
+        document.head.appendChild(style);
+      } else if (
+        currentWeather == "Smoke" ||
+        currentWeather == "Dust" ||
+        currentWeather == "Sand"
+      ) {
+        temp1 = "Conditions dusty, smoky, or sandy";
+        temp2 = "Expecting poor air quality due to smoke, dust, and sand";
+        bg = "./media/desert.gif";
+        const style = document.createElement("style");
+        style.innerHTML = `
+          body::before {
+            background-image: url('./media/smog.jpg');
+          }
+        `;
+        document.head.appendChild(style);
+      } else if (currentWeather == "Squall" || currentWeather == "Tornado") {
+        temp1 = "Windy with showers";
+        temp2 = "Gusty winds, rain, potential tornado";
+        bg = "./media/tornado.gif";
+        const style = document.createElement("style");
+        style.innerHTML = `
+          body::before {
+            background-image: url('./media/tornado-2.jpg');
+          }
+        `;
+        document.head.appendChild(style);
       }
+
       console.log(bg);
+      weatherData.innerHTML = "";
       weatherData.innerHTML = `
      <div
     class="card shadow rounded-3"
@@ -67,7 +147,7 @@ getWeatherData = async () => {
         ${temp2}
       </h5>
       <img
-        src="./media/rain-icon.png"
+        src="${weatherIcon}"
         alt=""
         class="ms-2 w-25 climate-img"
       />
@@ -84,4 +164,21 @@ getWeatherData = async () => {
     alert(`Please enter valid input`);
   }
 };
+bgArray = [
+  "./media/night.gif",
+  "./media/summer.gif",
+  "./media/winter.gif",
+  "./media/winter_morning.gif",
+  "./media/desert.gif",
+  "./media/tornado.gif",
+];
+let currentIndex = 0;
+changeBg = () => {
+  currentIndex = (currentIndex + 1) % bgArray.length;
+  climateCard.style.backgroundImage = `url(${bgArray[currentIndex]})`;
+};
+if (!isFunctionCalled) {
+  intervalId = setInterval(changeBg, 3000);
+}
+
 // https://api.openweathermap.org/data/2.5/weather?q=Kochi&appid=5fe36b192ffd1c36dffb6752bc1722b2
